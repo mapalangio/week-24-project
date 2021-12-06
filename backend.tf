bac907ac4e0fd5acee708fb42a7d53d74e0e371b
Mark Palangio <mark.palangio@igowarrior.com>
2021-12-06 17:47:06 -0500

    update backend.tf file.
    this is an update to the backend tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 # --- root/backend.tf ---
 
 terraform {
-  backend "s3" {
-    bucket = "week-24-project-07162021"
-    key    = "remote.tfstate"
-    region = "us-east-1"
+  backend "s3" {       
+    bucket = "igw-s3-general-bucket"
+    key = "Prod/week-24-project-120621.tfstate"
+    dynamodb_table = "s3-state-lock"
+    region = "ca-central-1"
   }
 }
